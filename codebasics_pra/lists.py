5ebe6452d0cdc5b33a0701c4934121e56d851e91
jeffgtx <indradev.g@gmail.com>
2016-10-23 19:44:40 +0530

    Added pop function

--- codebasics_pra/lists.py
+++ codebasics_pra/lists.py
@@ -14,3 +14,11 @@ print ("Adding one more item to list 1 ['brother'] ", lst1)
 print ("Adding one correction to list 2 [','] ", lst2)
 
 print ("Is 'Hi' Present in lst1 : {0}".format("Hi " in lst1) )
+
+
+fruits = ["apple", "bananna", "grapes"]
+
+fruits.insert(len(fruits), "Pears")
+
+pop_item = fruits.pop()
+print(pop_item)
